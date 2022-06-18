f84e8763422de62d2266bff68dc06b0a98ca89ba
Jerome <stemgh1999@yahoo.com>
2022-06-18 21:37:45 +0800

    0618

--- sport/src/index.js
+++ sport/src/index.js
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import Text from './text/text';
-import Citys from './text/bbb';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Citys />
+    <Text />
   </React.StrictMode>
 );
 
