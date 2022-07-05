93308def8d27e39eab6e27bc66bb49898d03af1f
DongYou <pipimorr@gmail.com>
2022-07-05 22:57:30 +0800

    0705

--- php/sportType.php
+++ php/sportType.php
@@ -1,16 +1,17 @@
 <?php
+    // SELECT DISTINCT type from lesson ORDER BY type;
+
     include('sql.php');
     header("Access-Control-Allow-Origin:*");
-    $sql = "SELECT img1,type,title,addr,price,cname,mode FROM `lesson` INNER JOIN coach ON lesson.cid = coach.cid";
+    $sql = "SELECT DISTINCT `type` from lesson ORDER BY `type` ";
     $result = $mysqli->query($sql);
     $i=0;
     $myJSON=[];
     while($datas = $result->fetch_object()){
-        // var_dump($datas);
-        $datas->img1 = base64_encode($datas->img1);
         $myJSON[$i] = $datas;
         $i++;
+        
     }
     $dataToClient=json_encode($myJSON);
-    echo $dataToClient;    
+    echo $dataToClient;   
 ?>
