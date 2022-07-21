277df340cecca0b350d786a6ac8b70dfbc930e3c
DongYou <pipimorr@gmail.com>
2022-07-21 17:24:26 +0800

    dong php

--- sport/src/php/DongPHP/form.php
+++ sport/src/php/DongPHP/form.php
@@ -1,9 +1,22 @@
 <?php
-    foreach($_FILES as $k=>$v){
-        echo "$k + $v <br />";
-    }
-    foreach($_POST as $k=>$v){
-        echo "$k + $v <br />";
-    }
+    // foreach($_FILES as $k=>$v){
+    //     var_dump($v);
+    // }
+    // foreach($_POST as $k=>$v){
+    //     echo "$k + $v <br />";
+    // }
+    header("Access-Control-Allow-Origin:*");
+    var_dump($_POST['imgFlag1']);
+    // echo"<br />";
+    var_dump($_FILES['img1']);
+    // echo"<br />";
+    var_dump($_POST['imgFlag2']);
+    // echo"<br />";
+    var_dump($_FILES['img2']);
+    // echo"<br />";
+    var_dump($_POST['imgFlag3']);
+    // echo"<br />";
+    var_dump($_FILES['img3']);
 ?>
+
 
